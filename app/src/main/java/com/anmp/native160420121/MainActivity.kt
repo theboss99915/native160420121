f9a71dc3918291b9cacff1240249d0622392370a
ASUS1 <paddyoop@gmail.com>
2023-03-01 14:48:16 +0700

    branch

--- app/src/main/java/com/anmp/native160420121/MainActivity.kt
+++ app/src/main/java/com/anmp/native160420121/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //test
-
+        //ini branch
         //ayo jerr lemot banget
         //ini udah ejremy
     }
